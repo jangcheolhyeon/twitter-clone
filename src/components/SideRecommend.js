4c61c6835bfba2005fd7960b1d984761521aa3e8
unknown <wkdcjfgus000@naver.com>
2022-11-17 16:50:21 +0900

    feat : topNavi, sideComponent 추가
    view를 위해 topNavi와 sideComponent를임시적으로 만들어 놓음

---
+++ src/components/SideRecommend.js
@@ -0,0 +1,10 @@
+
+const SideRecommend = () => {
+    return(
+        <div className="right_container">
+            <h1>SideRecommend</h1>
+        </div>
+    )
+}
+
+export default SideRecommend;
