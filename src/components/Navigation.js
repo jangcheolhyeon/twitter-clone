edec76536b8644b86879ae956bc4d998b31d26ff
unknown <wkdcjfgus000@naver.com>
2022-11-17 16:39:18 +0900

    style : navigation css style
    왼쪽 테마인 Navigation.js의 css를 수정했음

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faUserCircle, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = ({ userObj }) => {
-    const iconColor = "#04AAFF";
+    const iconColor = "white";
     const iconSize = "2x";
 
     return(
@@ -16,7 +16,33 @@ const Navigation = ({ userObj }) => {
                         <FontAwesomeIcon icon={faTwitter} color={iconColor} size={iconSize} />
                     </Link>
                 </li>
+                
                 <li>
+                    <div className="li_container">
+                        <Link to="/" className="nav_link">
+                            <FontAwesomeIcon icon={faHouse} color={iconColor} size={iconSize} />
+                            <span className="nav_link">HOME</span>
+                        </Link>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="li_container">
+                        <Link to="/" className="nav_link">
+                            <FontAwesomeIcon icon={faUser} color={iconColor} size={iconSize} />
+                            <span className="nav_link">Profile</span>
+                        </Link>
+                    </div>
+                </li>
+
+
+                <div className="navi_tweet_container">
+                    <div className="tweet_btn">
+                        <span>Tweet</span>
+                    </div>
+                </div>
+
+                {/* <li>
                     <Link to="/profile" className="nav_link_profile">
                         <FontAwesomeIcon icon={faUser} color={iconColor} size={iconSize} />
                         <span>
@@ -25,7 +51,16 @@ const Navigation = ({ userObj }) => {
                             : "Profile"}
                         </span>
                     </Link>
-                </li>
+                </li> */}
+
+                <div className="current_user_info_container">
+                    <FontAwesomeIcon icon={faUserCircle} color={iconColor} size={iconSize} />
+                    <span className="current_user_info">
+                        {userObj.displayName
+                        ? `${userObj.displayName}`
+                        : null}
+                    </span>
+                </div>
             </ul>
         </nav>
     );
