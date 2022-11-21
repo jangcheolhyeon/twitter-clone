7d307deb179a5f702202f3d4aa2083b6bd3637b5
unknown <wkdcjfgus000@naver.com>
2022-11-21 14:01:10 +0900

    style : proifle.js 주석 제거

--- src/Routers/Profile.js
+++ src/Routers/Profile.js
@@ -1,6 +1,5 @@
-import { getAuth, signOut, updateProfile } from 'firebase/auth';
+import { updateProfile } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { collection, getDocs, query, where, orderBy, updateDoc, doc } from "firebase/firestore";
 import { authService, db, storageService } from 'fbase';
 import { v4 } from "uuid";
@@ -23,34 +22,19 @@ const Profile = ({ userObj, refreshUserObj, usersProfile, setCurrentPage }) => {
         Likes : false
     });
 
-    const auth = getAuth();
-    const navi = useNavigate();
-
-    console.log("userObj", userObj);
-    console.log("usersProfile", usersProfile);
-
-    const onLogout = () => {
-        signOut(auth);
-        navi('/');
-    }
-
     const getMyTweets = async () => {
-        // db에 컬렉션이 tictoc에 userId가 userObj.uid와 일치하면 createdAt 내림차순으로 쿼리 가져오기
         const q = query(
             collection(db, "tictoc"),
             where("userId", "==", userObj.uid),
             orderBy("createdAt", "desc")
         );
         
-        // getDocs()메서드로 쿼리 결과 값 가져오기
         await getDocs(q);
 
     };
 
     const onDisplayNameClick = async(event) => {
         event.preventDefault();
-        console.log("click");
-        //다르면 업데이트
         onUpdateUserImg();
         if(newDisplayName !== userObj.displayName){
             await updateProfile(authService.currentUser, {displayName: newDisplayName});
@@ -78,7 +62,6 @@ const Profile = ({ userObj, refreshUserObj, usersProfile, setCurrentPage }) => {
         setCurrentPage('profile');
     }, []);
 
-    // OAuth대신 로그인 하면 displayName이 안나옴. 그래서 email의 @기준으로 앞 부분을 displayNAme으로 설정
     const createAccountUser = () => {
         if(userObj.displayName === null){
             userObj.displayName = userObj.email.split('@')[0];
@@ -188,40 +171,6 @@ const Profile = ({ userObj, refreshUserObj, usersProfile, setCurrentPage }) => {
                 {currentNavi.TweetsReplies && <ProfileNaviTweets_Replies usersProfile={usersProfile} userObj={userObj} />}
                 {currentNavi.Media && <ProfileNaviMedia usersProfile={usersProfile} userObj={userObj} />}
                 {currentNavi.Likes && <ProfileNaviLikes usersProfile={usersProfile} userObj={userObj} />}
-
-                {/* <div className='profile_follow_recommend'>
-                    <span>Who to follow</span>
-                    
-                    <div className="recommend_list_container">
-                        <ul className="recommend_list">
-                            {usersProfile.filter(element => element.userId !== userObj.uid).map((element) => {
-                                return(
-                                    <>
-                                        <RecommendFriend
-                                            key={element.userId} user={element} 
-                                        />
-                                        <span>asdlfnsaldk</span>
-                                    </>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                </div> */}
-
-                {/* <div className="profile_image">
-                    <img src={userAttachment} />
-                </div>
-                <label htmlFor="attach-file" className="factoryInput__label">
-                    <span>Add photo</span>
-                    <FontAwesomeIcon icon={faPlus} />
-                </label>
-                <input type="file" id="attach-file" accept='image/*' className='update_btn file_upload_input' onChange={onUserAttachment} />
-
-                <form onSubmit={onDisplayNameClick} className='profileForm' >
-                    <input type="text" placeholder="Display Name" value={newDisplayName} onChange={onChangeDisplayName} className='formInput' />
-                    <button className='formBtn'>update profile</button>
-                </form>
-                <button onClick={onLogout} className='formBtn cancelBtn logOut'>logout</button> */}
             </div>
         </>
     );
