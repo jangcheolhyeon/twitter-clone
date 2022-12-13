340c9bbce2063ab0ff4fc61d20aed2132de8d651
unknown <wkdcjfgus000@naver.com>
2022-12-13 15:39:47 +0900

    refact : Profile에서 user의 정보를 업데이트 하는 기능 분리
    Profile에서 user의 정보를 업데이트 하는 기능 분리
    user의 배경이미지, 유저이미지, 닉네임을 변경하는 기능을
    UpdateUserProfile.js로 분리함.
    
    style에서는 컾모넌트를 변경하면서 부모태그가 변경되면서 부모 클래스가
    변경되었기 때문에 container에 left가 딱 맞춰져있음.
    그래서 left: 0 을 추가함

--- src/Routers/Profile.js
+++ src/Routers/Profile.js
@@ -4,27 +4,21 @@ import { updateDoc, doc } from "firebase/firestore";
 import { authService, db, storageService } from 'fbase';
 import { v4 } from "uuid";
 import { getDownloadURL, ref, uploadString } from "@firebase/storage";
-import ProfileNaviTweets from 'components/Profile/ProfileNaviTweets';
-import ProfileNaviTweets_Replies from 'components/Profile/ProfileNaviTweets_Replies';
-import ProfileNaviMedia from 'components/Profile/ProfileNaviMedia';
-import ProfileNaviLikes from 'components/Profile/ProfileNaviLikes';
 import ModalUpdateProfile from 'components/Profile/ModalUpdateProfile';
 import UserHistory from 'components/Profile/UserHistory';
+import UpdateUserProfile from 'components/Profile/UpdateUserProfile';
 
 const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile, setCurrentPage, setToastAlert, setToastText, setUsersProfile, setTweetDetail }) => {
     const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
-    const [userAttachment, setUserAttachment] = useState(userObj.photoURL);
     const [userBackgroundAttachment, setUserBackgroundAttachment] = useState(null);
     const [changedUserBackgroundAttachment, setChangedUserBackgroundAttachment] = useState(null);
-    const [modalOpen, setModalOpen] = useState(false);
     const [myTweetList, setMyTweetList] = useState(null);
     const [tweets, setTweets] = useState([]);
-
-    let currentUser;
-
     const [followersCnt, setFollowersCnt] = useState(0);
     const [followingCnt, setFollowingCnt] = useState(0);
 
+    let currentUser;
+    
     const getMyTweets = () => {    
         setMyTweetList(messages.filter(element => {
             return element.userId === userObj.uid;
@@ -56,44 +50,8 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
         sortTweetList();
     }, [tweets])
 
-    const onChangeUserProfile = async(event) => {
-        event.preventDefault();
-        onUpdateUserImg();
-        onUpdateUserBackground();
-        if(newDisplayName !== userObj.displayName){
-            await updateProfile(authService.currentUser, {displayName: newDisplayName});
-            const currentUserId = usersProfile.filter(element => element.userId === userObj.uid)[0];
-
-            await updateDoc(doc(db, 'usersInfo', currentUserId.id), {
-                displayName : userObj.displayName
-            })
-                        
-            refreshUserObj();
-        }
-        setModalOpen((prev) => !prev);
-    }
-
-    useEffect(() => {
-        const newUsersProfile = usersProfile.map(element => {
-            if(element.userId === userObj.uid){
-                return {...element, displayName : userObj.displayName}
-            } else{
-                return element;
-            }
-        })
-        setUsersProfile(newUsersProfile);
-    }, [modalOpen])
-
     useEffect(() => {
         createAccountUser();
-        
-        if(userObj.photoURL !== '' || userObj.photoURL !== null){
-            setUserAttachment(userObj.photoURL);
-        }
-        
-        currentUser = usersProfile.filter(element => element.userId === userObj.uid)[0];
-        setUserBackgroundAttachment(currentUser.backgroundImg);
-        setChangedUserBackgroundAttachment(currentUser.backgroundImg);
 
         getMyTweets();
         if(!currentUser && usersProfile.length !== 0){
@@ -119,14 +77,8 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
 
     useEffect(() => {
         sortTweetList();
-    }, [usersProfile,])
-
-    const onChangeDisplayName = (event) => {
-        const {target : {value}} = event;
-        setNewDisplayName(value);
-    }
+    }, [usersProfile])
         
-
     useEffect(() => {
         if(usersProfile.length === 0) return;
         currentUser = usersProfile.filter(element => element.userId === userObj.uid)[0];
@@ -141,67 +93,6 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
         }
     }
 
-    const onUserAttachment = (e) => {
-        const {target : { files }} = e;
-        
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {currentTarget : {result}} = finishedEvent;
-            setUserAttachment(result);
-        }
-        reader.readAsDataURL(theFile);
-    }
-
-    const onUserBackgroundAttachment = (e) => {
-        const {target : {files}} = e;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {currentTarget : {result}} = finishedEvent;
-            setUserBackgroundAttachment(result);
-        }
-        reader.readAsDataURL(theFile);
-    }
-
-    const onUpdateUserImg = async() => {
-        let attachmentUrl = "";
-
-        const attachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
-        const response = await uploadString(attachmentRef, userAttachment, "data_url" );
-        attachmentUrl = await getDownloadURL(response.ref);
-
-        if(userAttachment !== userObj.userImage){
-            await updateProfile(authService.currentUser, {photoURL: attachmentUrl});
-            await updateDoc(doc(db, 'usersInfo', `${usersProfile.filter(element => element.userId === userObj.uid)[0].id}`), {
-                userImage : attachmentUrl,
-            });
-            refreshUserObj();
-        }
-
-    }
-
-    const onUpdateUserBackground = async() => {
-        let backAttachmentUrl = "";
-
-        const backAttachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
-        const backResponse = await uploadString(backAttachmentRef, userBackgroundAttachment, "data_url" );
-        backAttachmentUrl = await getDownloadURL(backResponse.ref);
-
-        if(userBackgroundAttachment !== userObj.backgroundImg){
-            await updateProfile(authService.currentUser, {backgroundImg: backAttachmentUrl});
-            await updateDoc(doc(db, 'usersInfo', `${usersProfile.filter(element => element.userId === userObj.uid)[0].id}`), {
-                backgroundImg : backAttachmentUrl,
-            });
-            refreshUserObj();
-        }
-        setChangedUserBackgroundAttachment(userBackgroundAttachment);
-    }
-
-    const handleUpdateProfile = () => {
-        setModalOpen((prev) => !prev);
-    }
-
     return(
         <>
             {myTweetList === null || usersProfile.length === 0 ? (
@@ -209,7 +100,6 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
                 </div>
             ) : (
                 <>
-                    { modalOpen && <ModalUpdateProfile userAttachment={userAttachment} onUserAttachment={onUserAttachment} onUserBackgroundAttachment={onUserBackgroundAttachment} newDisplayName={newDisplayName} onChangeDisplayName={onChangeDisplayName} onChangeUserProfile={onChangeUserProfile} setModalOpen={setModalOpen} userBackgroundAttachment={userBackgroundAttachment} />}
                     <div className='container'>
                         <div className='background_container'>
                             {userBackgroundAttachment && (
@@ -220,7 +110,18 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
                         <div className='my_profile_container'>
                             <div className='my_profile_container_top'>
                                 <img src={userObj.photoURL} />
-                                <button onClick={handleUpdateProfile}>Set up profile</button>
+                                <UpdateUserProfile 
+                                    userObj={userObj} 
+                                    usersProfile={usersProfile}
+                                    setUsersProfile={setUsersProfile}
+                                    userBackgroundAttachment={userBackgroundAttachment}
+                                    setUserBackgroundAttachment={setUserBackgroundAttachment}
+                                    setChangedUserBackgroundAttachment={setChangedUserBackgroundAttachment}
+                                    newDisplayName={newDisplayName}
+                                    setNewDisplayName={setNewDisplayName}
+                                    refreshUserObj={refreshUserObj}
+                            
+                                />
                             </div>
                         </div>
 
@@ -250,26 +151,7 @@ const Profile = ({ userObj, messages, currentPage, refreshUserObj, usersProfile,
                             setTweetDetail={setTweetDetail}
                             tweets={tweets}                        
                         />
-                        {/* <div className='my_trace_navi'>    
-                            <div className={currentNavi.Tweets ? "my_trace_box tweets tweets_active" : "my_trace_box tweets"} onClick={() => handleTraceClick('Tweets')}>
-                                <span>Tweets</span>
-                            </div>
-                            <div className={currentNavi.TweetsReplies ? 'my_trace_box tweets_replies tweets_replies_active' : 'my_trace_box tweets_replies'} onClick={() => handleTraceClick('TweetsReplies')}>
-                                <span>Tweets & replies</span>
-                            </div>
-                            <div className={currentNavi.Media ? 'my_trace_box media media_active' : 'my_trace_box media'} onClick={() => handleTraceClick('Media')}>
-                                <span>Media</span>
-                            </div>
-                            <div className={currentNavi.Likes ? 'my_trace_box likes likes_active' : 'my_trace_box likes'} onClick={() => handleTraceClick('Likes')}>
-                                <span>Likes</span>
-                            </div>
-                        </div>
 
-                        {currentNavi.Tweets && <ProfileNaviTweets usersProfile={usersProfile} userObj={userObj} tictoc={myTweetList} setToastAlert={setToastAlert} setToastText={setToastText} setUsersProfile={setUsersProfile} currentPage={currentPage} setCurrentPage={setCurrentPage} setTweetDetail={setTweetDetail} />}
-                        {currentNavi.TweetsReplies && <ProfileNaviTweets_Replies usersProfile={usersProfile} userObj={userObj} setUsersProfile={setUsersProfile} />}
-                        {currentNavi.Media && <ProfileNaviMedia usersProfile={usersProfile} userObj={userObj} />}
-                        {currentNavi.Likes && <ProfileNaviLikes userObj={userObj} tweets={tweets} usersProfile={usersProfile} setToastAlert={setToastAlert} setToastText={setToastText} />} */}
-                    
                     </div>
                 </>
             )}
---
+++ src/components/Profile/UpdateUserProfile.js
@@ -0,0 +1,128 @@
+import { authService, db, storageService } from "fbase";
+import { updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+import React, { useEffect, useState } from "react";
+import { v4 } from "uuid";
+import ModalUpdateProfile from "./ModalUpdateProfile";
+
+const UpdateUserProfile = ({ userObj, usersProfile, setUsersProfile, userBackgroundAttachment, setUserBackgroundAttachment, setChangedUserBackgroundAttachment, newDisplayName, setNewDisplayName, refreshUserObj }) => {   
+    const [modalOpen, setModalOpen] = useState(false);
+    const [userAttachment, setUserAttachment] = useState(userObj.photoURL);
+
+    let currentUser;
+
+    useEffect(() => {
+        if(userObj.photoURL !== '' || userObj.photoURL !== null){
+            setUserAttachment(userObj.photoURL);
+        }
+        
+        currentUser = usersProfile.filter(element => element.userId === userObj.uid)[0];
+        setUserBackgroundAttachment(currentUser.backgroundImg);
+        setChangedUserBackgroundAttachment(currentUser.backgroundImg);
+    }, [])
+
+    useEffect(() => {
+        const newUsersProfile = usersProfile.map(element => {
+            if(element.userId === userObj.uid){
+                return {...element, displayName : userObj.displayName}
+            } else{
+                return element;
+            }
+        })
+        setUsersProfile(newUsersProfile);
+    }, [modalOpen])
+
+    const handleUpdateProfile = () => {
+        setModalOpen((prev) => !prev);
+    }
+
+    const onChangeDisplayName = (event) => {
+        const {target : {value}} = event;
+        setNewDisplayName(value);
+    }
+
+    const onUserAttachment = (e) => {
+        const {target : { files }} = e;
+        
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget : {result}} = finishedEvent;
+            setUserAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+
+    const onUserBackgroundAttachment = (e) => {
+        const {target : {files}} = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget : {result}} = finishedEvent;
+            setUserBackgroundAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+
+
+    const onChangeUserProfile = async(event) => {
+        event.preventDefault();
+        onUpdateUserImg();
+        onUpdateUserBackground();
+        if(newDisplayName !== userObj.displayName){
+            await updateProfile(authService.currentUser, {displayName: newDisplayName});
+            const currentUserId = usersProfile.filter(element => element.userId === userObj.uid)[0];
+
+            await updateDoc(doc(db, 'usersInfo', currentUserId.id), {
+                displayName : userObj.displayName
+            })
+                        
+            refreshUserObj();
+        }
+        setModalOpen((prev) => !prev);
+    }
+
+    const onUpdateUserImg = async() => {
+        let attachmentUrl = "";
+
+        const attachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
+        const response = await uploadString(attachmentRef, userAttachment, "data_url" );
+        attachmentUrl = await getDownloadURL(response.ref);
+
+        if(userAttachment !== userObj.userImage){
+            await updateProfile(authService.currentUser, {photoURL: attachmentUrl});
+            await updateDoc(doc(db, 'usersInfo', `${usersProfile.filter(element => element.userId === userObj.uid)[0].id}`), {
+                userImage : attachmentUrl,
+            });
+            refreshUserObj();
+        }
+    }
+
+
+    const onUpdateUserBackground = async() => {
+        let backAttachmentUrl = "";
+
+        const backAttachmentRef = ref(storageService, `${userObj.uid}/${v4()}`);
+        const backResponse = await uploadString(backAttachmentRef, userBackgroundAttachment, "data_url" );
+        backAttachmentUrl = await getDownloadURL(backResponse.ref);
+
+        if(userBackgroundAttachment !== userObj.backgroundImg){
+            await updateProfile(authService.currentUser, {backgroundImg: backAttachmentUrl});
+            await updateDoc(doc(db, 'usersInfo', `${usersProfile.filter(element => element.userId === userObj.uid)[0].id}`), {
+                backgroundImg : backAttachmentUrl,
+            });
+            refreshUserObj();
+        }
+        setChangedUserBackgroundAttachment(userBackgroundAttachment);
+    }
+    
+    return(
+        <>
+            { modalOpen && <ModalUpdateProfile userAttachment={userAttachment} onUserAttachment={onUserAttachment} onUserBackgroundAttachment={onUserBackgroundAttachment} newDisplayName={newDisplayName} onChangeDisplayName={onChangeDisplayName} onChangeUserProfile={onChangeUserProfile} setModalOpen={setModalOpen} userBackgroundAttachment={userBackgroundAttachment} />}
+            <button onClick={handleUpdateProfile}>Set up profile</button>
+        </>
+    );
+}
+
+export default UpdateUserProfile;
