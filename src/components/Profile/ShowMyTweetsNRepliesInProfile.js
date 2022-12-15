5db44825fb1eabe64f795ad2655916543376b7da
unknown <wkdcjfgus000@naver.com>
2022-12-15 12:49:02 +0900

    fix : UI가 이상하게 나온는 것을 수정
    사용자가 tweet, reply, retweet을 작성하지 않은 경우에 UI를 수정함.
    삼항연산자 때문에 Tweet이 없는 경우에는 없다고만 보여줬는데 없을
    경우에도 친구추천과 같은 UI도 보여지도록 수정함

--- src/components/Profile/ShowMyTweetsNRepliesInProfile.js
+++ src/components/Profile/ShowMyTweetsNRepliesInProfile.js
@@ -5,62 +5,68 @@ import RecommendFriend from "components/Navigation/RecommendFriend";
 import Tweet from "components/Tweet";
 
 const ShowMyTweetsNReplies = ({ userObj, usersProfile, setUsersProfile, myTweetList, setToastAlert, setToastText, currentPage, setCurrentPage, setTweetDetail }) => {
+    console.log("myTweetList", myTweetList);
     return(
         <>
             {myTweetList.length === 0 || myTweetList === null ? (
-                <h1>Loading...</h1>
+                <div className="profile_empty_my_tweet_container">
+                    <h1>No Tweet, Reply, Retweet</h1>
+                </div>
             ) : (
                 <>
                     {myTweetList.map(element => {
                         return <Tweet key={element.createdAt} tictoc={element} isOwner={true} userObj={userObj} usersProfile={usersProfile} setToastAlert={setToastAlert} setToastText={setToastText} setTweetDetail={setTweetDetail} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
                     })}
-                    <div className='my_trace_content'>
-                        <div className='my_trace_content_top'>
-                            <span>Let's get you set up</span>
-                            <FontAwesomeIcon icon={faEllipsis} className='three-dots-icon' />
-                        </div>
-                        <div className='setup_container'>
-                            <div className="setup_box">
-                                <div>
-                                    <FontAwesomeIcon icon={faUser} />
-                                </div>
-                                <span>Complete your profile</span>
-                            </div>
-                            <div className="setup_box">
-                                <div>
-                                    <FontAwesomeIcon icon={faUserPlus} />
-                                </div>
-                                <span>Follow 5 accounts</span>
-                            </div>
-                            <div className="setup_box">
-                                <div>
-                                    <FontAwesomeIcon icon={faCommentDots} />
-                                </div>
-                                <span>Follow 3 topics</span>
-                            </div>
-                        </div>
-                    </div>
-        
-                    <div className='profile_follow_recommend'>
-                        <span>Who to follow</span>
-                        
-                        <div className="recommend_list_container">
-                            <ul className="recommend_list">
-                                {usersProfile.filter(element => element.userId !== userObj.uid).map((element) => {
-                                    return(
-                                        <>
-                                            <RecommendFriend
-                                                key={element.userId} user={element} userObj={userObj} usersProfile={usersProfile} setUsersProfile={setUsersProfile}
-                                            />
-                                            <span>asdlfnsaldk</span>
-                                        </>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                    </div>
                 </>
             )}
+
+            <>
+                <div className='my_trace_content'>
+                    <div className='my_trace_content_top'>
+                        <span>Let's get you set up</span>
+                        <FontAwesomeIcon icon={faEllipsis} className='three-dots-icon' />
+                    </div>
+                    <div className='setup_container'>
+                        <div className="setup_box">
+                            <div>
+                                <FontAwesomeIcon icon={faUser} />
+                            </div>
+                            <span>Complete your profile</span>
+                        </div>
+                        <div className="setup_box">
+                            <div>
+                                <FontAwesomeIcon icon={faUserPlus} />
+                            </div>
+                            <span>Follow 5 accounts</span>
+                        </div>
+                        <div className="setup_box">
+                            <div>
+                                <FontAwesomeIcon icon={faCommentDots} />
+                            </div>
+                            <span>Follow 3 topics</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='profile_follow_recommend'>
+                    <span>Who to follow</span>
+                    
+                    <div className="recommend_list_container">
+                        <ul className="recommend_list">
+                            {usersProfile.filter(element => element.userId !== userObj.uid).map((element) => {
+                                return(
+                                    <>
+                                        <RecommendFriend
+                                            key={element.userId} user={element} userObj={userObj} usersProfile={usersProfile} setUsersProfile={setUsersProfile}
+                                        />
+                                        <span>asdlfnsaldk</span>
+                                    </>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </>
         </>
     )
 }
